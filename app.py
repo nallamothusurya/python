b612095474999296e9f00169dd31f1768b6cca43
surya <ayyappanallamothu4@gmail.com>
2024-10-19 21:35:05 +0530

    version2

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
+
+# Route to serve the favicon.ico file
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/x-icon')
+
+@app.route('/author.png')
+def author_image():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'author.png')
 
 @app.route('/')
 def home():
@@ -8,14 +20,19 @@ def home():
     <html>
         <head>
             <title>Simple Web Page</title>
+            <link rel="icon" href="/favicon.ico" type="image/x-icon">
         </head>
         <body>
-            <h1>Welcome to my simple web page!</h1>
+            <h1>Welcome to my simple Python web page!</h1>
             <p>This is a simple page running on port 5050.</p>
-            <img src="author.png" alt="Surya Nallamothu" style="border: 2px solid black; border-radius: 5px;">
+            <img src="/author.png" alt="Surya Nallamothu" style="align-items:center" >
         </body>
     </html>
     '''
 
 if __name__ == '__main__':
+    # Create a static folder to serve your image and favicon
+    if not os.path.exists('static'):
+        os.makedirs('static')
+    
     app.run(host='0.0.0.0', port=5050)
